c4b3029e8f8856e0bfec2821b5a627bfe32a36f8
Pete Curtis <pmjcurtis@gmail.com>
2016-02-29 21:52:45 -0800

    Making sure I can see the main method

--- HelloWorld/src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by PeteCurtis on 2/29/16.
- */
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.print("Hello World");
-    }
-}
---
+++ UI/src/Main.java
@@ -0,0 +1,12 @@
+import static java.lang.System.out;
+
+/**
+ * Created by PeteCurtis on 2/29/16.
+ */
+public class Main {
+    static String greeting = "Hello World";
+
+    public static void  main(String[] args) {
+        out.print(greeting);
+    }
+}
